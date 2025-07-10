587e4611ee4104ebb02bc74444841b45d58267f2
prabhakar025 <prabhakar263801@gmail.com>
2025-07-11 03:17:11 +0530

    done

--- Array.js
+++ Array.js
@@ -1,1 +1,10 @@
-const evennumber=[0,2,4,8,10]
+// shift and unshift
+const evennumber=[0,2,4,6,8,10,14,16,18,20,22,24]
+const animal=['cow', 'dog', 'cat', 'rat']
+// evennumber.shift()
+// evennumber.unshift(0)
+
+//concat
+evennumber.concat(animal)
+const AddedArray=evennumber.concat(animal)
+const AddedArray2=animal.concat(evennumber)
