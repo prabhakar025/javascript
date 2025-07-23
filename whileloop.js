3730b1f84e333bef5f8e58a2b23606ee19965adf
prabhakar025 <prabhakar263801@gmail.com>
2025-07-23 19:21:00 +0530

    javascript

---
+++ whileloop.js
@@ -0,0 +1,24 @@
+console.log('program started')
+let num=1
+// print message
+
+// while(num<10){
+//     console.log('hii');
+//     num++
+// }
+
+// print 1 to 100
+
+// while (num<=100)
+// {
+//     console.log(num);
+// num=num+1
+// }
+const friend=['sukriti','sakshi','sumit','sakashm','suman','samata']
+let i=0
+
+while (i<friend.length) {
+console.log(`${i+1}.${friend[i]}`);
+i++    
+}
+console.log('program end!')
