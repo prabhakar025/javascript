752bcee634feb6d015344e71b054b15efc34f3f5
prabhakar025 <prabhakar263801@gmail.com>
2025-07-27 18:26:06 +0530

    accio

---
+++ conditional.js
@@ -0,0 +1,10 @@
+// >,<,>=,<=,!=,!==
+// let a= 30
+// let b=20
+// let ans=a<b
+// console.log(ans,typeof(ans))
+
+let a="30"
+let b=30
+let ans = a===b
+console.log(ans)
--- fixedno.js
+++ fixedno.js
@@ -1,4 +1,4 @@
 let n=12.1
 let ans=12.10
 let ans=n.toFixed(1)
-console.log()
+console.log(n)
