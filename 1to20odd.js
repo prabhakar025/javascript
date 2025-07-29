5cecf7784f2a6dcd6d2cd3bb6585b281f2e886c3
prabhakar025 <prabhakar263801@gmail.com>
2025-07-29 21:54:05 +0530

    done

---
+++ 1to20odd.js
@@ -0,0 +1,6 @@
+let num ;
+for(let i=1; i<=20; i++){
+    if(i%2 !== 0){
+        console.log(i)
+    }
+}
---
+++ stringreverse.js
@@ -0,0 +1,6 @@
+const name="prabhakar"
+let rev = ""
+for(let i=name.length-1; i >= 0; i--){
+    rev += name[i]
+}
+console.log(rev)
