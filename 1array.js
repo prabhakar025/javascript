0f8df6e556ef48b1365dc1219658b2af1c1b524b
prabhakar025 <prabhakar263801@gmail.com>
2025-07-29 21:37:19 +0530

    array

---
+++ 1array.js
@@ -0,0 +1,8 @@
+let marks=[94,98,76,55,79]
+console.log(marks)
+console.log(marks.length)
+// console.log(marks.indexOf[2])
+let newArray=marks.indexOf(marks[2])
+console.log(marks[2])
+let heroes=["amirkhan", "salu","anushka","deepika"]
+console.log(heroes)
