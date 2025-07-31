a0c5db7df72ee90c86d20fde3d45cc53699f0fb6
prabhakar025 <prabhakar263801@gmail.com>
2025-07-31 23:26:20 +0530

    switch

---
+++ switchopt.js
@@ -0,0 +1,14 @@
+const option=1;
+switch (option) {
+    case 1:
+        console.log('Namaste')
+        break;
+case 2:
+    console.log('hello')
+    break;
+    case 3:
+        console.log('banjor')
+    default:
+        console.log('invalid')
+        break;
+}
