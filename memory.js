90ab778f7589fb005e9af77c9b54bf514562a090
prabhakar025 <prabhakar263801@gmail.com>
2025-08-02 02:45:45 +0530

    stack heap

--- memory.js
+++ memory.js
@@ -4,3 +4,11 @@ let anotherName=myYoutubename
 anotherName="gora"
 console.log(myYoutubename);
 console.log(anotherName);
+let userOne={
+Email: "usergmail.com",
+upi:"userybl",
+}
+let userTwo=userOne
+userTwo.Email="prabhakar@gmail.com"
+console.log(userOne.Email)
+console.log(userTwo.Email)
