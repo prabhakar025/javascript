5ac3ea04913a86a01e89b7da98a68f1bbb6ad252
prabhakar025 <prabhakar263801@gmail.com>
2025-08-02 02:45:20 +0530

    stack heap

---
+++ memory.js
@@ -0,0 +1,6 @@
+// stack(Primitive),Heap(Non-Primitive)
+let myYoutubename="Brokeprabhakar";
+let anotherName=myYoutubename
+anotherName="gora"
+console.log(myYoutubename);
+console.log(anotherName);
