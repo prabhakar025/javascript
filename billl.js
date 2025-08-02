cf0998d3f431794418f5b91c43f4cb57cfb2b449
prabhakar025 <prabhakar263801@gmail.com>
2025-08-02 18:51:29 +0530

    accio class

---
+++ billl.js
@@ -0,0 +1,18 @@
+let units=300
+let bill= 0
+if (units<=50) {
+    bill=units*1//40
+}
+else if (units<=150) {
+    bill=50*1+(units-50)*2
+}
+else if (units<=250) {
+    bill=50*1 + 100*2 + (units-250)*3
+}
+else if (units>250) {
+    bill=50*1 +100*2 + 100*3 + (units-250)*4
+}
+if (bill>150) {
+    bill=bill+ bill*20/100
+}
+console.log(bill)
