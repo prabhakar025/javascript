29c19b49de9fcd5b188394f22850543e5be328f3
prabhakar025 <prabhakar263801@gmail.com>
2025-08-04 00:28:27 +0530

    chai aur code

---
+++ date_time.js
@@ -0,0 +1,10 @@
+// Date
+
+// let myDate=new Date()
+// console.log(myDate.toString())
+// console.log(myDate.toDateString())
+// console.log(myDate.toLocaleString())
+// console.log(typeof myDate)
+
+let myCreateDate=new Date(2025, 7, 03)
+console.log(myCreateDate.toDateString())
