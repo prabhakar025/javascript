3ccb8fd125d4d49c9c5333d862e216d7185b4fd7
prabhakar025 <prabhakar263801@gmail.com>
2025-08-03 18:23:38 +0530

    accio

---
+++ forloopset2.js
@@ -0,0 +1,28 @@
+// for(let i=10; i>=1; i=i/2)
+//     console.log(i)
+
+// for(let i=1; i<=5; i=i+1)
+//     console.log(i)
+// for(let i=1; i<=5; i++)
+//     console.log(i)
+// for(let i=1; i<=5; ++i)
+//     console.log(i)
+
+
+
+
+//  let x=1
+//  let y=1
+//  x++
+//  y++
+//  console.log(x)
+//  console.log(y)
+
+
+let x=1 
+console.log(x++)
+console.log(x)
+
+let y=1
+console.log(++y)
+console.log(y)
