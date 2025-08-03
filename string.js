d1a4877260e2f20daa01522e1fd9b4bd7c28c84e
prabhakar025 <prabhakar263801@gmail.com>
2025-08-03 23:04:50 +0530

    chai aur code

--- string.js
+++ string.js
@@ -2,3 +2,21 @@ const firstName="prabhakar"
 const lastName="kumar"
 //console.log(firstName + lastName);
 console.log(`My name is ${firstName} and My title is ${lastName}`);
+const gameName=new String("pkdhdkp")
+console.log(gameName[0])
+console.log(gameName.__proto__)
+console.log(gameName.length)
+console.log(gameName.toUpperCase())
+console.log(gameName.charAt(3))
+console.log(gameName.indexOf('h'))
+const newString=gameName.substring(0,3) 
+console.log(newString)
+const anotherString=gameName.slice(0,3)
+console.log(anotherString)
+const newStringOne="    prabhakar"
+console.log(newStringOne)
+console.log(newStringOne.trim())
+const url="https://prabhakar.com/prabhakar%20singh"
+console.log(url.replace('%20','-'))
+console.log(url.includes('kumar'))
+console.log(gameName.split('-'))
