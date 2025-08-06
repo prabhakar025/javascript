66cb4da761e1449e5a4a2a89f8e49e8e67ac9c37
prabhakar025 <prabhakar263801@gmail.com>
2025-08-06 22:26:09 +0530

    accio

---
+++ q3.js
@@ -0,0 +1,21 @@
+// Take a number and check if it is integer number or decimal number
+
+let n = 12.5 // 12 
+
+let integer_part = parseInt(n) // 12 
+
+if(n == integer_part){
+    console.log("integer number")
+}
+else{
+    console.log("Decinmal number")
+}
+
+// let decimal_part = n - integer_part // 0.5
+
+// if(decimal_part == 0){
+//     console.log("Integer")
+// }
+// else{
+//     console.log("Decimal")
+// }
