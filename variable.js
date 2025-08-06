8810a755bfbd4e3c75068be2ca924e55a0afb034
prabhakar025 <prabhakar263801@gmail.com>
2025-08-06 22:08:23 +0530

    accio

---
+++ variable.js
@@ -0,0 +1,34 @@
+// a = 10 
+// b = 20 
+
+// $sumOf2Numbers = a+b  // + symbol is of plus
+
+// console.log(sumOf2Numbers)
+
+// Rules => Captial Letter, small Letter, numbers, Special Character($, _)
+
+// Name cannot start with number
+
+
+
+
+a_b = 100 
+
+_ab = 40 
+
+_ = 50 
+
+_123 = 100 
+
+_A11$12 = 500 
+
+ram-11-b = 1000
+
+1ramesh = 100
+
+console.log(a_b)
+console.log(_ab)
+console.log(_)
+console.log(_123)
+console.log(_A11$12)
+console.log(ram-11-b)
