6eb9827f2faee8ca07cbe4ec4f508639a78d3365
prabhakar025 <prabhakar263801@gmail.com>
2025-08-06 22:21:45 +0530

    accio

---
+++ q5.js
@@ -0,0 +1,25 @@
+// Take a number and and print "Hello" if it div by 3, print World if it is div by 5 , if div by 15 print "Hello World"
+
+// let n = 47
+// if(n%15 ==0){
+//     console.log("hello World")
+// }
+// else if (n%3 ==0){
+//    console.log("Hello")
+// }
+// else if(n%5==0){
+//     console.log("World")
+// }
+
+
+let n = 15
+
+if(n%3 ==0){
+    console.log("Hello")
+}
+if(n%5==0){
+    console.log("World")
+}
+// if(n%15 ==0){
+//     console.log("hello World")
+// }
