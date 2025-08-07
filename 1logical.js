14fc308dbd76f5778be4bbfd8a98ec96ea4b5b1e
prabhakar025 <prabhakar263801@gmail.com>
2025-08-07 21:52:23 +0530

    accio

---
+++ 1logical.js
@@ -0,0 +1,36 @@
+// and(&&), or(||), !(not)
+
+
+// let a = 10
+// let b = 20 
+// let ans = a<b 
+
+// console.log(!ans)
+
+
+// &&:
+
+// let food = "tofu"
+// let ingred = "tel" 
+// let vegetable = "pyaz"
+
+// let ans = food == "paneer" && ingred=="butter" && vegetable=="pyaz"
+
+// console.log(ans)
+
+
+// 10<5 && console.log("Hello")
+
+
+// or : 
+
+// let drink1 = "pepsi"
+// let drink2 = "maaza"
+// let drink3 = "coke"
+
+// let ans = drink1 == "pepsi" || drink2 == "maaza" || drink3 == "coke"
+
+// console.log(ans)
+
+
+10<5 || console.log("Ramesh")
