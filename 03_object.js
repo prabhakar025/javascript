c5de9bc11894dd8db5abf92789e2dd996d802f6e
prabhakar025 <prabhakar263801@gmail.com>
2025-08-09 22:55:03 +0530

    chai aur code

---
+++ 03_object.js
@@ -0,0 +1,22 @@
+const course={
+    coursename:"js in hindi",
+    price:"999",
+    courseInstructor:"hitesh" 
+}
+//course.courseInstructor
+
+const{courseInstructor:instructor}=course
+console.log(instructor)
+
+
+// {
+//     "name": "prabhakar",
+//     "coursename": "js in hindi",
+//     "price":"free"
+// }
+
+[
+    {},
+    {},
+    {},
+]
