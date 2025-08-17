0098d63bb777af11f8b75eb0c5201170d803564f
prabhakar025 <prabhakar263801@gmail.com>
2025-08-17 16:47:46 +0530

    done

---
+++ commonfactor.js
@@ -0,0 +1,14 @@
+let a=45
+let b=60
+// for(let i=1;i<=a;i++){
+//     if(a%i==0 && b%i==0){
+// console.log(i)
+// break
+//     }
+// }
+
+for(let i=a;i>=1;i--){
+    if(a%i==0 && b%i==0){
+        console.log(i)
+    }
+}
