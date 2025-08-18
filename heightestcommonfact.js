fd7f9856fdc788e2867bb0d69781a02529993857
prabhakar025 <prabhakar263801@gmail.com>
2025-08-18 23:35:05 +0530

    done

---
+++ heightestcommonfact.js
@@ -0,0 +1,8 @@
+// let n=5
+// for(let i=1;i<=10;i++){
+//     console.log(n*i)
+//}
+
+
+let p=20
+let q=30
---
+++ largest.js
@@ -0,0 +1,9 @@
+let a = 15, b = 28, c = 19;
+
+if (a >= b && a >= c) {
+    console.log("Largest:", a);
+} else if (b >= a && b >= c) {
+    console.log("Largest:", b);
+} else {
+    console.log("Largest:", c);
+}
