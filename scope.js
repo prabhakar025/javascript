f742513f2b43056099d3d9648c3fdaeb4122cc68
prabhakar025 <prabhakar263801@gmail.com>
2025-08-19 07:19:19 +0530

    done

--- scope.js
+++ scope.js
@@ -1,9 +1,48 @@
-{
+//var c=30
+let a=20
+if(true){
 let a=10
 const b=20
-var c=30
-}
+//console.log("INNER:", a)
 
+}
+// for(let i=0;i<array.length;i++){
+//     const element=array[i];
+// }
 //console.log(a);
 //console.log(b);
-console.log(c);
+//console.log(c);
+
+
+
+function one() {
+    const username="prabhakar"
+    function two(){
+        const website="you tube"
+        console.log(username);
+    }
+   // console.log(website);
+   two()
+}
+// one()
+
+if(true){
+    const username="prabhakar"
+    if(username=="prabhakar"){
+        const website="youtube"
+        //console.log(username+ website);
+    }
+    // console.log(website);
+}
+//console.log(username);
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+console.log(addone(5))
+function addone(num){
+    return num+1
+}
+
+console.log(addtwo(6))
+const addtwo=function(num){
+    return num+2
+}
