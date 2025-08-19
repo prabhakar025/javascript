571c7c85495d60f5134bdb4ce1ff7e0dfe636ea2
prabhakar025 <prabhakar263801@gmail.com>
2025-08-19 15:26:30 +0530

    chai

---
+++ one.js
@@ -0,0 +1,26 @@
+// if satatement 
+// const isuserloggedIn=true
+// const temperature=90
+
+// if (temperature===70) {
+//     console.log("less tham 50");
+// }else{
+// console.log("temperatue is greater than 50");
+// }
+
+//<,>,<=,>=,==,!=,===,!==
+
+
+// const score=200
+// if (score>100) {
+//     const power=  "fly"
+//     console.log(`User power:${power}`);
+// }
+//     console.log(`User power:${power}`);
+
+
+//const balance=1000
+// if (balance>500) console.log("test"),console.log("test2");
+// 
+
+
