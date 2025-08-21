e24ba5449353506d993223e74473e91d862ca08b
prabhakar025 <prabhakar263801@gmail.com>
2025-08-21 10:19:07 +0530

    chai

--- one.js
+++ one.js
@@ -21,6 +21,15 @@
 
 //const balance=1000
 // if (balance>500) console.log("test"),console.log("test2");
-// 
+// if (balance<500) {
+//     console.log("less than");
+    
+// }else if (balance<750) {
+//     console.log("less than 750");
+// }else if (balance<900) {
+//     console.log("less than 750");
+// }else{
+//     console.log("less than 1200");
+// }
 
 
