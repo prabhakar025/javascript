78d9857d6517bf0641c7657261db21281e27315e
prabhakar025 <prabhakar263801@gmail.com>
2025-08-21 21:27:50 +0530

    accio

---
+++ whileLoopSet.js
@@ -0,0 +1,48 @@
+//Take a number and print its even factors:
+
+// let n=106
+// let i=1
+// while (i <= n) {
+//     if (n % i === 0 && i % 2 === 0) {
+//         console.log("Even Factor is",i);
+//     }
+//     i++;
+    
+// }
+// let n=106
+// for(let i=1; i<=n;i++){
+//     if (n%i==0 && i%2==0) {
+//         console.log("Even Factor is",i)
+//     }
+// }
+
+
+// let i=1
+// while (i<=5) {
+//     if (i==3) {
+//         break
+
+//     }
+//     console.log(i)//1
+    
+// }
+
+
+// let i=1
+// while (i<=5) {
+//     // console.log("AAAAA")
+//       i++
+//     if (i==3) {
+//         continue
+//     }
+//     console.log(i)//2,4,5,6
+  
+// }
+
+
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++
+
+//Take a number and display all its digits, 345=>3,4,5
+
+
