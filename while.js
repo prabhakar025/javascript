69576d3bc206f1980c976e185ca00cfe312258ea
prabhakar025 <prabhakar263801@gmail.com>
2025-08-21 20:55:34 +0530

    accio

---
+++ while.js
@@ -0,0 +1,26 @@
+
+
+
+// let i=1
+// for( ;i<=3; ){
+//     console.log(i)//1,2,3
+// }
+// i=i+1;//2,3,4
+
+// let i=1
+// for( ; ; ){
+// if(i==4){
+//     break
+// }
+// console.log(i)
+// i=i+1
+// }
+
+// 
+let i=1
+while (i<=3) {
+    console.log(i)
+        i=i+1
+    
+}
+
