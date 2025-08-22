7b5379d35203f5170198bbde96c039651cae1c94
prabhakar025 <prabhakar263801@gmail.com>
2025-08-22 18:50:15 +0530

    chai aur code

---
+++ truthy.js
@@ -0,0 +1,36 @@
+const userEmail=[]
+if (userEmail) {
+    console.log("Got user email");
+}else{
+    console.log("Don't have user email");
+}
+
+//+++++++++++++++++++++   falsy value  ++++++++++++
+
+// false, 0,-0,BigInt 0n,"",null,undefined,NaN
+
+// truthy values
+
+// truth=>  "0",'false'," ",[],{},function(){}
+//  if (userEmail.length===0) {
+//     console.log("Array is Empty");
+//  }
+
+const emptyObj={}
+if(Object.keys(emptyObj).length==0){
+     console.log("Object is Empty");
+}
+// Nullish coalescing Operator(??): null undefined
+let val1;
+// val1 =5 ?? 10
+// val1 = null ?? 10
+// val1 = undefined ?? 15
+val1 = null ?? 10  ?? 20
+console.log(val1);
+
+
+// ternary operator
+//  condition ? true : false
+
+const iceTeaprice=100
+iceTeaprice<= 80? console.log("less than 80"):console.log("more than 80 ")
