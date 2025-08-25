8c66cdae5bd610df93cc7f179fcb403cab0a5f74
prabhakar025 <prabhakar263801@gmail.com>
2025-08-25 21:48:35 +0530

    chai aur code

---
+++ map_filter.js
@@ -0,0 +1,17 @@
+// const coding=["js","ruby","java","python","cpp"]
+
+// const value=coding.forEach( (item)=> {
+// //console.log(item);
+// return item
+// })
+// console.log(value);
+
+
+const myNums=[1,2,3,4,5,6,7,8,9,10]
+//const newNums=myNums.filter( (num)=> num > 4)
+const newNums=myNums.filter((num)=>{
+return num >4// use return
+})
+// console.log(newNums);
+
+
