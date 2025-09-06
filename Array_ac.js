cf406d97af71002cbfb81a8aa332d1f4d24c118c
prabhakar025 <prabhakar263801@gmail.com>
2025-09-06 16:42:38 +0530

    accio

---
+++ Array_ac.js
@@ -0,0 +1,28 @@
+// let a=10
+// let b=20
+// let c=30
+
+
+// let arr=[10,20,30,40,50,60]
+// console.log(arr)
+
+
+// let arr=[10,true,'apple',false,10.5]
+// console.log(arr)
+// // length=> size of array
+// console.log(arr.length)
+// // index=> counting from 0
+// console.log(arr[0])
+
+
+let arr=[10,20,30,40,50,60]
+// for(i=0;i<arr.length-1;i++){
+// console.log(i,arr[i])
+// }
+
+
+let i=0
+while (i<arr.length-1) {
+    console.log(i,arr[i])
+    i++;
+}
