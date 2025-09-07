7c27b63e0be8f1efae860888dc42794186a1293d
prabhakar025 <prabhakar263801@gmail.com>
2025-09-07 19:00:52 +0530

    accio

---
+++ subArray.js
@@ -0,0 +1,16 @@
+// let arr=[10,20,30,40,50,60,70,80,90,100]
+
+// console.log(arr.slice(2,5))
+
+// console.log(arr.slice(-4,-1))
+
+// console.log(arr.slice(-8,4))
+
+// console.log(arr.slice(5,2))
+
+let arr=[10,20,30,40,50,60,70]
+// print all 3 length subarray
+
+// 10m20m30
+
+console.log(arr.slice(1,4))
