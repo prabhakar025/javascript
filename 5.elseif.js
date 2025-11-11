a1c2d353c6aa7bb60eef6937301477bf100b500a
prabhakar025 <prabhakar263801@gmail.com>
2025-11-11 13:10:54 +0530

    done

---
+++ 5.elseif.js
@@ -0,0 +1,50 @@
+//n=> positive , negative, zero;
+// let n=-100
+// if(n>100){
+//     console.log("positive")
+// }
+// else if (n<0) {
+//     console.log("negative")
+// }
+// else{
+//     console.log("zero")
+// }
+
+
+// let n=-10
+// if (n>0) {
+//     console.log("Positive")
+// }
+// else if (n==0) {
+//     console.log("Zero")
+// }
+// else{
+//     console.log("Negative")
+// }
+
+
+// let n=-10
+// if (n>0) {
+//     console.log("Positive")
+// }
+// else if (n==0) {
+//     console.log("Zero")
+// }
+
+
+
+// let x=40
+// if(x%2==1){
+//     console.log("A")
+// }
+
+
+let y=56
+// all number are true except 0.
+if(y){
+    console.log("A")
+}
+else{
+    console.log("B")
+}
+
