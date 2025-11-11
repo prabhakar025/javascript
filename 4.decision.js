502520d7a7721cbd546f19cb1c74599266d37e66
prabhakar025 <prabhakar263801@gmail.com>
2025-11-11 12:17:39 +0530

    done

---
+++ 4.decision.js
@@ -0,0 +1,30 @@
+// if it positive or  not positive
+
+// let n=100
+
+// if ((n>0)== true) 
+    // {
+//     console.log("Its a positive number")
+// }
+// else {
+
+//     console.log("Its not a positive number")
+// }
+
+
+// let v1= 120
+// if((v1-100)>20){
+//     console.log("Hello")
+// }
+// else{
+//     console.log("hey")
+// }
+
+
+let p2=100
+if(p2%100==0){
+    console.log("A")
+}
+else{
+    console.log("B")
+}
