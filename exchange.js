02be3aff29d8abc14804c4bf2196d7db9336aca0
prabhakar025 <prabhakar263801@gmail.com>
2025-11-11 09:34:32 +0530

    accio

--- exchange.js
+++ exchange.js
@@ -1,6 +1,18 @@
-let a=10
-let b=20
-let t=a
-a=b
-b=t
+//using with 3rd variable
+
+// let a=10
+// let b=20
+// let t=a
+// a=b
+// b=t
+// console.log(a,b)
+
+
+//using without 3rd variable
+
+a=10
+b=20
+a=a+b
+b=a-b
+a=a-b
 console.log(a,b)
