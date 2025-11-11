97e26c6340480154bed602fc81f3d4d5283bc76d
prabhakar025 <prabhakar263801@gmail.com>
2025-11-11 09:54:09 +0530

    done

---
+++ 1.conditional.js
@@ -0,0 +1,21 @@
+// > ,< , >=, <= , ==, ===, !=, !==
+// let a=10
+// let b=20
+// let ans=a<b
+// console.log(ans,typeof(ans))
+
+// let a=20
+// let ans =a==30
+// console.log(a)
+// console.log(ans)
+
+
+let a='30'  //string
+let b=30  //number
+
+let ans= a!==b
+console.log(ans)
+
+//  ==, !=
+
+//  ===, !==
