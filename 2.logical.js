a918ad806026940a70e8bb5ed3b0cbd128404bd0
prabhakar025 <prabhakar263801@gmail.com>
2025-11-11 10:44:26 +0530

    done

--- 2.logical.js
+++ 2.logical.js
@@ -1,6 +1,19 @@
 // &&;
-let food ="paneer"
-let ingerd="butter"
-let vegitable="pyaz"
-let ans=food=="paneer"&& ingerd=="butter" && vegitable=="pyaz"
-console.log(ans)
+
+
+// let food ="paneer"
+// let ingerd="butter"
+// let vegitable="pyaz"
+// let ans=food=="paneer"&& ingerd=="butter" && vegitable=="pyaz"
+// console.log(ans)
+
+
+// or:
+
+// let drink1="pepsi"
+// let drink2="maaza"
+// let  drink3="cock"
+// let ans= drink1=="pepsi"|| drink2=="maaza" ||drink3=="cock"
+// console.log(ans)
+
+10>5 || console.log("prabhakar")
