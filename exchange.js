f240d5a2a5c40c617f6cafb87a3548a2471cd3fe
prabhakar025 <prabhakar263801@gmail.com>
2025-11-11 09:22:28 +0530

    class2Accio

---
+++ exchange.js
@@ -0,0 +1,6 @@
+let a=10
+let b=20
+let t=a
+a=b
+b=t
+console.log(a,b)
---
+++ tofixed.js
@@ -0,0 +1,13 @@
+// let n=123.78766336728
+// let newN=n*100
+// let ans=parseInt(newN)/100
+// console.log(ans)
+
+// let n=12.7000
+// console.log(n)
+
+
+let n=125.363553
+//decimal 2 digits
+let ans=n.toFixed(2)
+console.log(ans)
