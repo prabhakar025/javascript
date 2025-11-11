fa1eab2b1c866169f857cad12128bd3960e77053
prabhakar025 <prabhakar263801@gmail.com>
2025-11-11 13:57:50 +0530

    done

---
+++ 6.questiuon.js
@@ -0,0 +1,34 @@
+// take a number and check if it is div by 5 or not 
+
+// let n=51
+// if (n%5==0) {
+//     console.log("It is divisible by 5")
+// }
+// else{
+//     console.log("Its is not divisible by 5")
+// }
+
+
+
+// take a number and check it is integer number or decimal number
+
+
+let n=12.5
+let integer_part=parseInt(n) //12
+let decimal_part=n-integer_part //0.5
+if (decimal_part==0)
+{
+    console.log("Integer")
+}
+else{
+    console.log("decimal")
+}
+
+//take two number and find the greatest among them
+
+
+// take three numbers and find the graetest among them
+
+
+
+// take a number and print "Hello" if it div by 3 ,print "World" if it is div by 5 ,if div  by 15 print "Hello World" 
